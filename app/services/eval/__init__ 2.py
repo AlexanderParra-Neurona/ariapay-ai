64788f23aaceeb40bacc7581917c08f40fb1b2ff
ssabrut <michael.gunawan2002@gmail.com>
2026-09-21 15:52:39 +0700

    chore: remove stray duplicate eval files

--- app/services/eval/__init__ 2.py
+++
@@ -1,7 +0,0 @@
-from app.services.eval.judge import (
-    get_judge_embeddings,
-    get_judge_llm,
-    get_ragas_metrics,
-)
-
-__all__ = ["get_judge_embeddings", "get_judge_llm", "get_ragas_metrics"]
